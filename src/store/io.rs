b7413ea41f57c85744350e2a8307b4e7e64f617a
Robert Habermeier <rphmeier@gmail.com>
2024-06-19 13:28:38 -0500

    add a fake IO worker that completes requests in random order

--- src/store/io.rs
+++ src/store/io.rs
@@ -1,6 +1,7 @@
 use super::{Page, Store, PAGE_SIZE};
 use crossbeam_channel::{Receiver, Sender, TryRecvError};
 use io_uring::{cqueue, opcode, squeue, types, IoUring};
+use rand::{Rng, prelude::SliceRandom};
 use slab::Slab;
 use std::{
     os::fd::AsRawFd,
@@ -11,10 +12,10 @@ use std::{
     time::{Duration, Instant},
 };
 
-const RING_CAPACITY: u32 = 32;
+const RING_CAPACITY: u32 = 64;
 
 // max number of inflight requests is bounded by the slab.
-const MAX_IN_FLIGHT: usize = 32;
+const MAX_IN_FLIGHT: usize = RING_CAPACITY as usize;
 
 static IO_OPS: AtomicU64 = AtomicU64::new(0);
 
@@ -104,81 +105,16 @@ fn run_worker(
         .expect("Error building io_uring");
 
     let (submitter, mut submit_queue, mut complete_queue) = ring.split();
+    let mut stats = Stats::new();
 
-    const LOG_DURATION: Duration = Duration::from_millis(200);
-
-    let mut iter = 0;
-    let mut last_log = Instant::now();
-    let mut total_inflight_us = 0;
-    let mut total_syscall_ns = 0;
-    let mut total_complete_ns = 0;
-    let mut total_submit_ns = 0;
-    let mut completions = 0;
-    let mut arrivals = 0;
-    let mut empty_count = 0;
-    let mut full_count = 0;
     loop {   
-        let elapsed = last_log.elapsed();
-        if elapsed > LOG_DURATION {
-            last_log = Instant::now();
-            let arrival_rate = arrivals as f64 * 1000.0 / elapsed.as_millis() as f64;
-            let average_inflight = total_inflight_us as f64 / completions as f64;
-            println!("full={full_count} empty={empty_count} iterations of {iter}");
-            println!("syscall_time={}ns completion_time={}ns submit_time={}ns", 
-                (total_syscall_ns as f64 / iter as f64) as usize,
-                (total_complete_ns as f64 / iter as f64) as usize,
-                (total_submit_ns as f64 / iter as f64) as usize,
-            );
-            println!("arrivals={} (rate {}/s) completions={} avg_inflight={}us | {}ms",
-                arrivals,
-                arrival_rate as usize,
-                completions,
-                average_inflight as usize,
-                elapsed.as_millis(),
-            );
-            println!("  estimated-QD={}", (arrival_rate * average_inflight / 1_000_000.0) as usize);
+        stats.log();
 
-            total_inflight_us = 0;
-            total_syscall_ns = 0;
-            total_complete_ns = 0;
-            total_submit_ns = 0;
-            completions = 0;
-            arrivals = 0;
-            full_count = 0;
-            empty_count = 0;
-            iter = 0;
-        }
-
-        iter += 1;
-        
         // 1. process completions.
-        let complete_start = Instant::now();
-
-        // uncomment to complete I/O after deadline instead of io-uring
-        {
-            const SEND_BACK_DURATION: Duration = Duration::from_micros(100);
-            for i in 0..MAX_IN_FLIGHT {
-                if pending.get(i).map_or(false, |i| i.start.elapsed() > SEND_BACK_DURATION) {
-                    let inflight = pending.remove(i);
-                    
-                    completions += 1;
-                    total_inflight_us += inflight.start.elapsed().as_micros();
-                    
-                    let command = inflight.command;
-                    let handle_idx = command.handle;
-                    let complete = CompleteIo { command: command, result: Ok(()) };
-                    if let Err(_) = handle_tx[handle_idx].send(complete) {
-                        // TODO: handle?
-                        break;
-                    }
-                }
-            }
-        }
-
         if !pending.is_empty() {
             complete_queue.sync();
             while let Some(completion_event) = complete_queue.next() {
-                if !pending.get(completion_event.user_data() as usize).is_some() {
+                if pending.get(completion_event.user_data() as usize).is_none() {
                     continue
                 }
                 let PendingIo {
@@ -186,8 +122,7 @@ fn run_worker(
                     start,
                 } = pending.remove(completion_event.user_data() as usize);
 
-                total_inflight_us += start.elapsed().as_micros();
-                completions += 1;
+                stats.note_completion(start.elapsed().as_micros() as u64);
 
                 let handle_idx = command.handle;
                 let result = if completion_event.result() == -1 {
@@ -195,26 +130,19 @@ fn run_worker(
                 } else {
                     Ok(())
                 };
-                let complete = CompleteIo { command, result: Ok(()) };
+                let complete = CompleteIo { command, result };
                 if let Err(_) = handle_tx[handle_idx].send(complete) {
                     // TODO: handle?
                     break;
                 }
             }
         }
-        total_complete_ns += complete_start.elapsed().as_nanos();
 
         // 2. accept new I/O requests when slab has space & submission queue is not full.
         let mut to_submit = false;
         let mut ios = 0;
 
-        let submit_start = Instant::now();
         submit_queue.sync();
-        if pending.len() == MAX_IN_FLIGHT {
-            full_count += 1;
-        } else if pending.is_empty() {
-            empty_count += 1;
-        }
         while pending.len() < MAX_IN_FLIGHT && !submit_queue.is_full() {
             let next_io = if pending.is_empty() {
                 // block on new I/O if nothing in-flight.
@@ -229,19 +157,7 @@ fn run_worker(
                     Err(TryRecvError::Disconnected) => break, // TODO: wait on pending I/O?
                 }
             };
-            arrivals += 1;
-
-            // uncomment to test what happens if we don't I/O
-            // {
-            //     let handle_idx = next_io.handle;
-            //     let complete = CompleteIo { command: next_io, result: Ok(()) };
-            //     if let Err(_) = handle_tx[handle_idx].send(complete) {
-            //         // TODO: handle?
-            //         break;
-            //     }
-
-            //     break
-            // }
+            stats.note_arrival();
 
             to_submit = true;
             let pending_index = pending.insert(PendingIo {
@@ -265,17 +181,61 @@ fn run_worker(
             submit_queue.sync();
         }
 
-        total_submit_ns += submit_start.elapsed().as_nanos();
-
         let wait = if pending.len() == MAX_IN_FLIGHT {
             1
         } else {
             0
         };
 
-        let syscall_start = Instant::now();
         submitter.submit_and_wait(wait).unwrap();
-        total_syscall_ns += syscall_start.elapsed().as_nanos();
+    }
+}
+
+struct Stats {
+    last_log: Instant,
+    total_inflight_us: u64,
+    completions: usize,
+    arrivals: usize,
+}
+
+impl Stats {
+    fn new() -> Self {
+        Stats {
+            last_log: Instant::now(),
+            total_inflight_us: 0,
+            completions: 0,
+            arrivals: 0,
+        }
+    }
+
+    fn note_completion(&mut self, inflight_us: u64) {
+        self.completions += 1;
+        self.total_inflight_us += inflight_us;
+    }
+
+    fn note_arrival(&mut self) {
+        self.arrivals += 1;
+    }
+
+    fn log(&mut self) {
+        const LOG_DURATION: Duration = Duration::from_millis(400);
+
+        let elapsed = self.last_log.elapsed();
+        if elapsed < LOG_DURATION {
+            return
+        }
+
+        self.last_log = Instant::now();
+        let arrival_rate = self.arrivals as f64 * 1000.0 / elapsed.as_millis() as f64;
+        let average_inflight = self.total_inflight_us as f64 / self.completions as f64;
+        println!("arrivals={} (rate {}/s) completions={} avg_inflight={}us | {}ms",
+            self.arrivals,
+            arrival_rate as usize,
+            self.completions,
+            average_inflight as usize,
+            elapsed.as_millis(),
+        );
+        println!("  estimated-QD={}", (arrival_rate * average_inflight / 1_000_000.0) as usize);
     }
 }
 
@@ -297,5 +257,99 @@ fn submission_entry(command: &mut IoCommand, store: &Store) -> squeue::Entry {
         .build(),
         IoKind::Fsync => opcode::Fsync::new(types::Fd(store.store_file.as_raw_fd()))
             .build(),
+    }
+}
+
+/// Create a fake I/O worker receiving requests and completing them in a random order with a
+/// given deadline.
+pub fn start_fake_io_worker(
+    _store: Arc<Store>,
+    num_handles: usize,
+) -> (Sender<IoCommand>, Vec<Receiver<CompleteIo>>) {
+    // main bound is from the pending slab.
+    let (command_tx, command_rx) = crossbeam_channel::bounded(MAX_IN_FLIGHT * 2);
+    let (handle_txs, handle_rxs) = (0..num_handles)
+        .map(|_| crossbeam_channel::unbounded())
+        .unzip();
+    let _ = std::thread::Builder::new()
+        .name("io_worker".to_string())
+        .spawn(move || run_fake_worker(command_rx, handle_txs))
+        .unwrap();
+    (command_tx, handle_rxs)
+}
+
+struct FakePendingIo {
+    command: IoCommand,
+    duration: u64,
+    end: Instant,
+}
+
+fn possible_latencies(latency_occurrences: Vec<(u64, usize)>) -> Vec<u64> {
+    latency_occurrences.into_iter().flat_map(|(l, x)| std::iter::repeat(l).take(x)).collect()
+}
+
+fn run_fake_worker(
+    command_rx: Receiver<IoCommand>,
+    handle_tx: Vec<Sender<CompleteIo>>,
+) {
+    // EV = ~319us with this distribution.
+    let possible_latencies = possible_latencies(vec![
+        (50, 1),
+        (75, 1),
+        (150, 2),
+        (250, 5),
+        (300, 10),
+        (400, 5),
+        (500, 2),
+        (600, 1),
+        (700, 1),
+    ]);
+
+    let mut stats = Stats::new();
+    let mut pending: Slab<FakePendingIo> = Slab::with_capacity(MAX_IN_FLIGHT);
+    let mut rng = rand::thread_rng();
+
+    loop {
+        stats.log();
+
+        let now = Instant::now();
+        for i in 0..MAX_IN_FLIGHT {
+            if pending.get(i).map_or(false, |i| i.end <= now) {
+                let item = pending.remove(i);
+
+                stats.note_completion(item.duration as u64);
+
+                let command = item.command;
+                let handle_idx = command.handle;
+                let complete = CompleteIo { command, result: Ok(()) };
+                if let Err(_) = handle_tx[handle_idx].send(complete) {
+                    // TODO: handle?
+                    break;
+                }
+            }
+        }
+
+        if pending.len() == MAX_IN_FLIGHT { continue }
+        let next_io = if pending.is_empty() {
+            match command_rx.recv() {
+                Ok(io) => io,
+                Err(_) => break,
+            }
+        } else {
+            match command_rx.try_recv() {
+                Ok(io) => io,
+                Err(TryRecvError::Disconnected) => break,
+                Err(TryRecvError::Empty) => continue,
+            }
+        };
+
+        let duration = *possible_latencies.choose(&mut rng).unwrap();
+        let end = Instant::now() + Duration::from_micros(duration);
+        pending.insert(FakePendingIo {
+            command: next_io,
+            duration,
+            end,
+        });
+        stats.note_arrival();
     }
 }
